0d12bff0849bd11341b3a3fe72350221f7b2e7ee
Toshit Chawda <r58playz@gmail.com>
2024-11-03 17:33:23 -0800

    make ring compile with optimizations

--- client/build.sh
+++ client/build.sh
@@ -18,7 +18,7 @@ if [ "$(wasm-bindgen -V)" != "$WBG" ]; then
 	exit 1
 fi
 
-RUSTFLAGS='-Zlocation-detail=none' cargo build --target wasm32-unknown-unknown -Z build-std=panic_abort,std -Z build-std-features=panic_immediate_abort,optimize_for_size --release $CARGOFLAGS "$@"
+CFLAGS='-O3' RUSTFLAGS='-Zlocation-detail=none' cargo build --target wasm32-unknown-unknown -Z build-std=panic_abort,std -Z build-std-features=panic_immediate_abort,optimize_for_size --release $CARGOFLAGS "$@"
 echo "[epx] cargo finished"
 wasm-bindgen --target web --out-dir out/ ../target/wasm32-unknown-unknown/release/epoxy_client.wasm
 echo "[epx] wasm-bindgen finished"
