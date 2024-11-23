5e54465e58826f7193751db726cf4c48a2b1d568
Toshit Chawda <r58playz@gmail.com>
2024-11-22 22:39:13 -0800

    stop using nightly rust

--- server/src/stream.rs
+++ server/src/stream.rs
@@ -44,6 +44,66 @@ pub enum ClientStream {
 	Invalid,
 }
 
+
+// taken from rust 1.82.0
+fn ipv4_is_global(addr: &Ipv4Addr) -> bool {
+	!(addr.octets()[0] == 0 // "This network"
+            || addr.is_private()
+            || (addr.octets()[0] == 100 && (addr.octets()[1] & 0b1100_0000 == 0b0100_0000)) // || addr.is_shared()
+            || addr.is_loopback()
+            || addr.is_link_local()
+            // addresses reserved for future protocols (`192.0.0.0/24`)
+            // .9 and .10 are documented as globally reachable so they're excluded
+            || (
+                addr.octets()[0] == 192 && addr.octets()[1] == 0 && addr.octets()[2] == 0
+                && addr.octets()[3] != 9 && addr.octets()[3] != 10
+            )
+            || addr.is_documentation()
+            || (addr.octets()[0] == 198 && (addr.octets()[1] & 0xfe) == 18) // || addr.is_benchmarking()
+            || (addr.octets()[0] & 240 == 240) // || addr.is_reserved()
+            || addr.is_broadcast())
+}
+fn ipv6_is_global(addr: &Ipv6Addr) -> bool {
+	!(
+		addr.is_unspecified()
+            || addr.is_loopback()
+            // IPv4-mapped Address (`::ffff:0:0/96`)
+            || matches!(addr.segments(), [0, 0, 0, 0, 0, 0xffff, _, _])
+            // IPv4-IPv6 Translat. (`64:ff9b:1::/48`)
+            || matches!(addr.segments(), [0x64, 0xff9b, 1, _, _, _, _, _])
+            // Discard-Only Address Block (`100::/64`)
+            || matches!(addr.segments(), [0x100, 0, 0, 0, _, _, _, _])
+            // IETF Protocol Assignments (`2001::/23`)
+            || (matches!(addr.segments(), [0x2001, b, _, _, _, _, _, _] if b < 0x200)
+                && !(
+                    // Port Control Protocol Anycast (`2001:1::1`)
+                    u128::from_be_bytes(addr.octets()) == 0x2001_0001_0000_0000_0000_0000_0000_0001
+                    // Traversal Using Relays around NAT Anycast (`2001:1::2`)
+                    || u128::from_be_bytes(addr.octets()) == 0x2001_0001_0000_0000_0000_0000_0000_0002
+                    // AMT (`2001:3::/32`)
+                    || matches!(addr.segments(), [0x2001, 3, _, _, _, _, _, _])
+                    // AS112-v6 (`2001:4:112::/48`)
+                    || matches!(addr.segments(), [0x2001, 4, 0x112, _, _, _, _, _])
+                    // ORCHIDv2 (`2001:20::/28`)
+                    // Drone Remote ID Protocol Entity Tags (DETs) Prefix (`2001:30::/28`)`
+                    || matches!(addr.segments(), [0x2001, b, _, _, _, _, _, _] if b >= 0x20 && b <= 0x3F)
+                ))
+            // 6to4 (`2002::/16`) – it's not explicitly documented as globally reachable,
+            // IANA says N/A.
+            || matches!(addr.segments(), [0x2002, _, _, _, _, _, _, _])
+            || ((addr.segments()[0] == 0x2001) && (addr.segments()[1] == 0xdb8)) // || addr.is_documentation()
+            || (addr.segments()[0] & 0xfe00) == 0xfc00 // addr.is_unique_local()
+            || (addr.segments()[0] & 0xfe00) == 0xfc00
+		// || addr.is_unicast_link_local()
+	)
+}
+fn is_global(addr: &IpAddr) -> bool {
+	match addr {
+		IpAddr::V4(x) => ipv4_is_global(x),
+		IpAddr::V6(x) => ipv6_is_global(x),
+	}
+}
+
 pub enum ResolvedPacket {
 	Valid(ConnectPacket),
 	NoResolvedAddrs,
@@ -100,8 +160,8 @@ impl ClientStream {
 				return Ok(ResolvedPacket::Blocked);
 			}
 
-			if (addr.is_global() && !CONFIG.stream.allow_global)
-				|| (!addr.is_global() && !CONFIG.stream.allow_non_global)
+			if (is_global(&addr) && !CONFIG.stream.allow_global)
+				|| (!is_global(&addr) && !CONFIG.stream.allow_non_global)
 			{
 				return Ok(ResolvedPacket::Blocked);
 			}
